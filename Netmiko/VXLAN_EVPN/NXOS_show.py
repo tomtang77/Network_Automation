436b676955561fb60a144cea70f85604b21887d0
Tom Tang <tom.tang@me.com>
2021-08-26 17:12:15 +0800

    VXLAN_EVPN

---
+++ Netmiko/VXLAN_EVPN/NXOS_show.py
@@ -0,0 +1,25 @@
+# from netmiko import Netmiko
+#
+# connection = Netmiko(host="10.10.1.10", port="22", username="cisco", password="cisco", device_type="cisco_ios")
+
+from netmiko import ConnectHandler
+cisco_device = {
+    "device_type": "cisco_nxos",
+    "host": "192.168.31.105",
+    "username": "cisco",
+    "password": "cisco",
+    "port": "22",
+    "secret": "cisco",
+    "verbose": True
+}
+
+connection = ConnectHandler(**cisco_device)
+prompt = connection.find_prompt()
+print(prompt)
+connection.enable()   #enter enable mode
+
+output = connection.send_command("show ip int bri")
+print(output)
+
+print("Closing connection")
+connection.disconnect()
